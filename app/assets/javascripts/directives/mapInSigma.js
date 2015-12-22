46cec3def4347c5c9cc720b03d4c5f24211309de
Paul Sauer <paulrsauer@gmail.com>
2015-12-22 02:22:33 -0500

    bug fix for deaths opacity in sigma ctrl

--- app/assets/javascripts/directives/mapInSigma.js
+++ app/assets/javascripts/directives/mapInSigma.js
@@ -150,7 +150,7 @@ function (d3Service,   $window,   DataService,   ControlsData) {
                         .attr('class', 'deaths')
                         .style("fill","#000000")
                         .attr("filter", "url(#blur)")
-                        .attr("opacity", ControlsData.show_deaths ? 2 : 0);
+                        .attr("opacity", ControlsData.show_deaths ? 0.2 : 0);
 
 
             }
